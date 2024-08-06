bb596581425a897b536d85ae0a7287ce1d366e9f
Francois Drielsma <francois.drielsma@gmail.com>
2024-08-06 15:51:19 -0700

    Support not having end predictions in PPN analysis script

--- spine/post/reco/ppn.py
+++ spine/post/reco/ppn.py
@@ -29,7 +29,7 @@ class PPNProcessor(PostBase):
             'ppn_masks': True, 'ppn_classify_endpoints': False}
 
     def __init__(self, assign_to_particles=False, restrict_shape=False,
-                 match_threshold=2., obj_type='particle', **ppn_pred_cfg):
+                 match_threshold=2., **ppn_pred_cfg):
         """Store the `get_ppn_predictions` keyword arguments.
 
         Parameters
@@ -49,6 +49,7 @@ class PPNProcessor(PostBase):
             Update result dictionary containing 'ppn_candidates' key
         """
         # Intialize the parent class
+        obj_type = 'particle' if assign_to_particles else None
         super().__init__(obj_type, 'reco')
 
         # Store the relevant parameters
