67bc5fbaaa5e1c94785cb5b50a658365a898a91f
Francois Drielsma <francois.drielsma@gmail.com>
2025-02-28 11:45:40 -0800

    Revamp builders to work on pure truth information (no full chain run at all)

--- spine/build/particle.py
+++ spine/build/particle.py
@@ -39,7 +39,7 @@ class ParticleBuilder(BuilderBase):
 
     # Necessary/optional data products to build a truth object
     _build_truth_keys = (
-            ('particles', False),
+            ('particles', False), ('truth_fragments', False),
             *BuilderBase._build_truth_keys
     )
 
@@ -69,7 +69,7 @@ class ParticleBuilder(BuilderBase):
                     particle_start_points, particle_end_points,
                     particle_group_pred, particle_node_type_pred,
                     particle_node_primary_pred, particle_node_orient_pred=None,
-                    reco_fragments=None, sources=None):
+                    sources=None, reco_fragments=None):
         """Builds :class:`RecoParticle` objects from the full chain output.
 
         Parameters
@@ -94,10 +94,10 @@ class ParticleBuilder(BuilderBase):
             (P, 2) Particle primary classification logits
         particle_node_orient_pred : np.ndarray, optional
             (P, 2) Particle orientation classification logits
-        reco_fragments : List[RecoFragment], optional
-            (F) List of reconstructed fragments
         sources : np.ndarray, optional
             (N, 2) Tensor which contains the module/tpc information
+        reco_fragments : List[RecoFragment], optional
+            (F) List of reconstructed fragments
 
         Returns
         -------
@@ -161,26 +161,19 @@ class ParticleBuilder(BuilderBase):
         """
         return self._build_truth(**data)
 
-    def _build_truth(self, label_tensor, label_adapt_tensor, particles, points,
-                     depositions, points_label, depositions_label,
-                     depositions_q_label=None, label_g4_tensor=None,
-                     points_g4=None, depositions_g4=None, sources=None,
-                     sources_label=None):
+    def _build_truth(self, particles, label_tensor, points_label,
+                     depositions_label, depositions_q_label=None,
+                     label_adapt_tensor=None, points=None, depositions=None,
+                     label_g4_tensor=None, points_g4=None, depositions_g4=None,
+                     sources_label=None, sources=None, truth_fragments=None):
         """Builds :class:`TruthParticle` objects from the full chain output.
 
         Parameters
         ----------
-        label_tensor : np.ndarray
-            Tensor which contains the cluster labels of each deposition
-        label_adapt_tensor : np.ndarray
-            Tensor which contains the cluster labels of each deposition,
-            adapted to the semantic segmentation prediction.
         particles : List[Particle]
             List of true particles
-        points : np.ndarray
-            (N, 3) Set of deposition coordinates in the image
-        depositions : np.ndarray
-            (N) Set of deposition values
+        label_tensor : np.ndarray
+            Tensor which contains the cluster labels of each deposition
         points_label : np.ndarray
             (N', 3) Set of deposition coordinates in the label image (identical
             for pixel TPCs, different if deghosting is involved)
@@ -188,17 +181,26 @@ class ParticleBuilder(BuilderBase):
             (N') Set of true deposition values in MeV
         depositions_q_label : np.ndarray, optional
             (N') Set of true deposition values in ADC, if relevant
+        label_adapt_tensor : np.ndarray, optional
+            Tensor which contains the cluster labels of each deposition,
+            adapted to the semantic segmentation prediction.
+        points : np.ndarray, optional
+            (N, 3) Set of deposition coordinates in the image
+        depositions : np.ndarray, optional
+            (N) Set of deposition values
         label_tensor_g4 : np.ndarray, optional
             Tensor which contains the cluster labels of each deposition
             in the Geant4 image (before the detector simulation)
-        points_g4 : np.ndarray
+        points_g4 : np.ndarray, optional
             (N'', 3) Set of deposition coordinates in the Geant4 image
-        depositions_g4 : np.ndarray
+        depositions_g4 : np.ndarray, optional
             (N'') Set of deposition values in the Geant4 image
-        sources : np.ndarray, optional
-            (N, 2) Tensor which contains the module/tpc information
         sources_label : np.ndarray, optional
             (N', 2) Tensor which contains the label module/tpc information
+        sources : np.ndarray, optional
+            (N, 2) Tensor which contains the module/tpc information
+        truth_fragments : List[TruthFragment], optional
+            (F) List of true fragments
 
         Returns
         -------
@@ -247,13 +249,14 @@ class ParticleBuilder(BuilderBase):
             if sources_label is not None:
                 particle.sources = sources_label[index]
 
-            index_adapt = np.where(
-                    label_adapt_tensor[:, GROUP_COL] == group_id)[0]
-            particle.index_adapt = index_adapt
-            particle.points_adapt = points[index_adapt]
-            particle.depositions_adapt = depositions[index_adapt]
-            if sources is not None:
-                particle.sources_adapt = sources[index_adapt]
+            if label_adapt_tensor is not None:
+                index_adapt = np.where(
+                        label_adapt_tensor[:, GROUP_COL] == group_id)[0]
+                particle.index_adapt = index_adapt
+                particle.points_adapt = points[index_adapt]
+                particle.depositions_adapt = depositions[index_adapt]
+                if sources is not None:
+                    particle.sources_adapt = sources[index_adapt]
 
             if label_g4_tensor is not None:
                 index_g4 = np.where(
@@ -320,19 +323,16 @@ class ParticleBuilder(BuilderBase):
         """
         return self._load_truth(**data)
 
-    def _load_truth(self, truth_particles, points, depositions, points_label,
-                    depositions_label, depositions_q_label=None, points_g4=None,
-                    depositions_g4=None, sources=None, sources_label=None):
+    def _load_truth(self, truth_particles, points_label, depositions_label,
+                    depositions_q_label=None, points=None, depositions=None,
+                    points_g4=None, depositions_g4=None, sources_label=None,
+                    sources=None):
         """Construct :class:`TruthParticle` objects from their stored versions.
 
         Parameters
         ----------
         truth_particles : List[TruthParticle]
             (P) List of partial truth particles
-        points : np.ndarray
-            (N, 3) Set of deposition coordinates in the image
-        depositions : np.ndarray
-            (N) Set of deposition values
         points_label : np.ndarray
             (N', 3) Set of deposition coordinates in the label image (identical
             for pixel TPCs, different if deghosting is involved)
@@ -340,14 +340,18 @@ class ParticleBuilder(BuilderBase):
             (N') Set of true deposition values in MeV
         depositions_q_label : np.ndarray, optional
             (N') Set of true deposition values in ADC, if relevant
-        points_g4 : np.ndarray
+        points : np.ndarray, optional
+            (N, 3) Set of deposition coordinates in the image
+        depositions : np.ndarray, optional
+            (N) Set of deposition values
+        points_g4 : np.ndarray, optional
             (N'', 3) Set of deposition coordinates in the Geant4 image
-        depositions_g4 : np.ndarray
+        depositions_g4 : np.ndarray, optional
             (N'') Set of deposition values in the Geant4 image
-        sources : np.ndarray, optional
-            (N, 2) Tensor which contains the module/tpc information
         sources_label : np.ndarray, optional
             (N', 2) Tensor which contains the label module/tpc information
+        sources : np.ndarray, optional
+            (N, 2) Tensor which contains the module/tpc information
 
         Returns
         -------
@@ -368,10 +372,11 @@ class ParticleBuilder(BuilderBase):
             if sources_label is not None:
                 particle.sources = sources_label[particle.index]
 
-            particle.points_adapt = points[particle.index_adapt]
-            particle.depositions_adapt = depositions[particle.index_adapt]
-            if sources is not None:
-                particle.sources_adapt = sources[particle.index_adapt]
+            if points is not None:
+                particle.points_adapt = points[particle.index_adapt]
+                particle.depositions_adapt = depositions[particle.index_adapt]
+                if sources is not None:
+                    particle.sources_adapt = sources[particle.index_adapt]
 
             if points_g4 is not None:
                 particle.points_g4 = points_g4[particle.index_g4]
